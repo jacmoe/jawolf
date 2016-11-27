eafe2ecbfaee74447e7e6c02c07430f5f91ab651
Jacob Moen <jacmoe.dk@gmail.com>
2016-11-28 00:18:17 +0100

    Formatting according to editorconfig

--- src/main.c
+++ src/main.c
@@ -20,60 +20,60 @@
 
 void window_size_callback(GLFWwindow *window, int width, int height)
 {
-  glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
 
 int main()
 {
-  GLFWwindow *window = 0;
+    GLFWwindow *window = 0;
 
-  /* Initialize the API */
-  if (!glfwInit())
-  {
-    return EXIT_FAILURE;
-  }
-
-  /* Initialize OpenGL context flags */
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  /* Compute window resolution from the main monitor's */
-  {
-    const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-    const int width = mode->width / 2;
-    const int height = mode->height / 2;
-
-    /* Create the window and OpenGL context */
-    window = glfwCreateWindow(width, height, "demo", NULL, NULL);
-    if (!window)
+    /* Initialize the API */
+    if (!glfwInit())
     {
-      fprintf(stderr, "error: failed to create window.\n");
-      glfwTerminate();
-      return EXIT_FAILURE;
+        return EXIT_FAILURE;
     }
-    glfwSetWindowSizeCallback(window, window_size_callback);
-  }
 
-  /* Make the window's context current */
-  glfwMakeContextCurrent(window);
+    /* Initialize OpenGL context flags */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  /* Initialize app specific environment */
-  AppSetup();
+    /* Compute window resolution from the main monitor's */
+    {
+        const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        const int width = mode->width / 2;
+        const int height = mode->height / 2;
 
-  /* Mainloop */
-  while (!glfwWindowShouldClose(window))
-  {
-    /* Update and render one frame */
-    AppFrame();
+        /* Create the window and OpenGL context */
+        window = glfwCreateWindow(width, height, "demo", NULL, NULL);
+        if (!window)
+        {
+            fprintf(stderr, "error: failed to create window.\n");
+            glfwTerminate();
+            return EXIT_FAILURE;
+        }
+        glfwSetWindowSizeCallback(window, window_size_callback);
+    }
 
-    /* Swap front & back buffers */
-    glfwSwapBuffers(window);
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-    /* Manage events */
-    glfwPollEvents();
-  }
+    /* Initialize app specific environment */
+    AppSetup();
 
-  glfwTerminate();
-  return EXIT_SUCCESS;
+    /* Mainloop */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Update and render one frame */
+        AppFrame();
+
+        /* Swap front & back buffers */
+        glfwSwapBuffers(window);
+
+        /* Manage events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
--- src/opengl.c
+++ src/opengl.c
@@ -22,130 +22,130 @@
 
 static int checkExtensions(const char **extensions)
 {
-  unsigned int i = 0u;
-  int valid = 1;
+    unsigned int i = 0u;
+    int valid = 1;
 
-  for (i = 0u; extensions[i] != NULL; ++i)
-  {
-    if (!glfwExtensionSupported(extensions[i]))
+    for (i = 0u; extensions[i] != NULL; ++i)
     {
-      fprintf(stderr, "warning : Extension \"%s\" is not supported.\n", extensions[i]);
-      valid = 0;
+        if (!glfwExtensionSupported(extensions[i]))
+        {
+            fprintf(stderr, "warning : Extension \"%s\" is not supported.\n", extensions[i]);
+            valid = 0;
+        }
     }
-  }
-  return valid;
+    return valid;
 }
 
 /* ========================================================================== */
 
 extern void InitGL()
 {
-  /* ~TODO : too app specifics atm */
-  const char *s_extensions[] = {
-      "GL_ARB_compute_shader",
-      "GL_ARB_separate_shader_objects",
-      "GL_ARB_shader_image_load_store",
-      NULL};
+    /* ~TODO : too app specifics atm */
+    const char *s_extensions[] = {
+        "GL_ARB_compute_shader",
+        "GL_ARB_separate_shader_objects",
+        "GL_ARB_shader_image_load_store",
+        NULL};
 
-  /* Check if specific extensions exists */
-  checkExtensions(s_extensions);
+    /* Check if specific extensions exists */
+    checkExtensions(s_extensions);
 
-  /* Load GLEW */
-  glewExperimental = GL_TRUE;
-  GLenum result = glewInit();
+    /* Load GLEW */
+    glewExperimental = GL_TRUE;
+    GLenum result = glewInit();
 
-  /* flush doubtful error */
-  glGetError();
+    /* flush doubtful error */
+    glGetError();
 
-  if (GLEW_OK != result)
-  {
-    fprintf(stderr, "Error: %s\n", glewGetErrorString(result));
-  }
+    if (GLEW_OK != result)
+    {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(result));
+    }
 }
 
 void CheckShaderStatus(GLuint shader)
 {
-  GLint status = 0;
+    GLint status = 0;
 
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-  if (status != GL_TRUE)
-  {
-    char buffer[1024];
-    glGetShaderInfoLog(shader, 1024, 0, buffer);
-    fprintf(stderr, "%s\n", buffer);
-  }
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status != GL_TRUE)
+    {
+        char buffer[1024];
+        glGetShaderInfoLog(shader, 1024, 0, buffer);
+        fprintf(stderr, "%s\n", buffer);
+    }
 }
 
 void CheckProgramStatus(GLuint program)
 {
-  GLint status = 0;
+    GLint status = 0;
 
-  glGetProgramiv(program, GL_LINK_STATUS, &status);
-  if (status != GL_TRUE)
-  {
-    char buffer[1024];
-    glGetProgramInfoLog(program, 1024, 0, buffer);
-    fprintf(stderr, "%s\n", buffer);
-  }
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (status != GL_TRUE)
+    {
+        char buffer[1024];
+        glGetProgramInfoLog(program, 1024, 0, buffer);
+        fprintf(stderr, "%s\n", buffer);
+    }
 
-  glValidateProgram(program);
-  glGetProgramiv(program, GL_VALIDATE_STATUS, &status);
-  if (status != GL_TRUE)
-  {
-    fprintf(stderr, "Program failed to be validated.\n");
-    exit(EXIT_FAILURE);
-  }
+    glValidateProgram(program);
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &status);
+    if (status != GL_TRUE)
+    {
+        fprintf(stderr, "Program failed to be validated.\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 static const char *GetErrorString(GLenum err)
 {
 #define STRINGIFY(x) #x
-  switch (err)
-  {
-  case GL_NO_ERROR:
-    return STRINGIFY(GL_NO_ERROR);
+    switch (err)
+    {
+    case GL_NO_ERROR:
+        return STRINGIFY(GL_NO_ERROR);
 
-  case GL_INVALID_ENUM:
-    return STRINGIFY(GL_INVALID_ENUM);
+    case GL_INVALID_ENUM:
+        return STRINGIFY(GL_INVALID_ENUM);
 
-  case GL_INVALID_VALUE:
-    return STRINGIFY(GL_INVALID_VALUE);
+    case GL_INVALID_VALUE:
+        return STRINGIFY(GL_INVALID_VALUE);
 
-  case GL_INVALID_OPERATION:
-    return STRINGIFY(GL_INVALID_OPERATION);
+    case GL_INVALID_OPERATION:
+        return STRINGIFY(GL_INVALID_OPERATION);
 
-  case GL_STACK_OVERFLOW:
-    return STRINGIFY(GL_STACK_OVERFLOW);
+    case GL_STACK_OVERFLOW:
+        return STRINGIFY(GL_STACK_OVERFLOW);
 
-  case GL_STACK_UNDERFLOW:
-    return STRINGIFY(GL_STACK_UNDERFLOW);
+    case GL_STACK_UNDERFLOW:
+        return STRINGIFY(GL_STACK_UNDERFLOW);
 
-  case GL_OUT_OF_MEMORY:
-    return STRINGIFY(GL_OUT_OF_MEMORY);
+    case GL_OUT_OF_MEMORY:
+        return STRINGIFY(GL_OUT_OF_MEMORY);
 
-  default:
-    return "GetErrorString : Unknown constant";
-  }
+    default:
+        return "GetErrorString : Unknown constant";
+    }
 #undef STRINGIFY
 
-  return "";
+    return "";
 }
 
 void CheckGLError(const char *file, const int line, const char *errMsg, int bExitOnFail)
 {
-  GLenum err = glGetError();
+    GLenum err = glGetError();
 
-  if (err != GL_NO_ERROR)
-  {
-    fprintf(stderr,
-            "OpenGL error @ \"%s\" [%d] : %s [%s].\n",
-            file, line, errMsg, GetErrorString(err));
-
-    if (bExitOnFail != 0)
+    if (err != GL_NO_ERROR)
     {
-      exit(EXIT_FAILURE);
+        fprintf(stderr,
+                "OpenGL error @ \"%s\" [%d] : %s [%s].\n",
+                file, line, errMsg, GetErrorString(err));
+
+        if (bExitOnFail != 0)
+        {
+            exit(EXIT_FAILURE);
+        }
     }
-  }
 }
 
 /* ========================================================================== */
