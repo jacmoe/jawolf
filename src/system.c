1aae924bda3f652c3ddfb8b1be7d1c1d7e4a383c
Jacob Moen <jacmoe.dk@gmail.com>
2016-12-01 21:32:07 +0100

    Gui moved out - not ready yet (reverted from commit 7c5730ca1992c55d08f9c15308ec155553a7af68)

--- src/system.c
+++ src/system.c
@@ -1,164 +1,167 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#define GLEW_STATIC
+//#include <GL/glew.h>
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
+#include "geometry.h"
 #include "buffer.h"
 #include "system.h"
+#include "dbg.h"
+
+static const char *vertexSource = "#version 330 core\n"
+"\n"
+"in vec2 position;\n"
+"in vec2 texcoord;\n"
+"\n"
+"out vec3 Color;\n"
+"out vec2 Texcoord;\n"
+"\n"
+"void main() {\n"
+"    Texcoord = texcoord;\n"
+"    gl_Position = vec4(position, 0.0, 1.0);\n"
+"}";
+
+static const char *fragmentSource = "#version 330 core\n"
+"\n"
+"in vec2 Texcoord;\n"
+"\n"
+"out vec4 outColor;\n"
+"\n"
+"uniform sampler2D tex;\n"
+"\n"
+"void main() {\n"
+"    outColor = texture(tex, Texcoord);\n"
+"}";
+
+// Flags
+static int resizef;
 
 
-static char *ReadFile(const char *path) {
-    FILE *f = fopen(path, "r");
-    if (!f) {
-        fprintf(stderr, "Coudn't read file: '%s'\n", path);
+void system_fullscreen(int flag) {
+
+}
+
+
+void s_mouse_grab(int flag) {
+    
+}
+
+
+void system_init(const char *title, int width, int height) {
+
+}
+
+
+void system_quit() {
+
+}
+
+
+Tick system_tick_get() {
+
+    Tick t = {0};
+
+    return t;
+}
+
+
+uint32_t system_blit(Buffer *buf) {
+    uint32_t start = system_time_get();
+
+    // if (resizef) {
+    //     resizef = 0;
+
+    //     int winwidth, winheight;
+    //     //SDL_GetWindowSize(window, &winwidth, &winheight);
+
+    //     glViewport(0, 0, winwidth, winheight);
+    // }
+
+    // glTexSubImage2D(
+    //         GL_TEXTURE_2D,
+    //         0, 0,
+    //         0,
+    //         buf->width, buf->height,
+    //         GL_RGBA, GL_UNSIGNED_BYTE,
+    //         buf->pixels
+    //         );
+
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    //SDL_GL_SwapWindow(window);
+
+    return system_time_get() - start;
+}
+
+
+Vector system_mousepos_get(Buffer *buf) {
+    int mx, my;
+    int winwidth = 0;
+    int winheight = 0;
+    return (Vector){ mx * buf->width / winwidth, my * buf->height / winheight };
+}
+
+
+// http://sdl.beuc.net/sdl.wiki/Pixel_Access
+uint32_t pixel_get(SDL_Surface *surface, int x, int y) {
+    int bpp = surface->format->BytesPerPixel;
+    // Here p is the address to the pixel we want to retrieve.
+    uint8_t *p = (uint8_t *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch (bpp) {
+        case 1:
+            return *p;
+            break;
+
+        case 2:
+            return *(uint16_t *)p;
+            break;
+
+        case 3:
+            if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+                return p[0] << 16 | p[1] << 8 | p[2];
+            } else {
+                return p[0] | p[1] << 8 | p[2] << 16;
+            }
+            break;
+
+        case 4:
+            return *(uint32_t *)p;
+            break;
+
+        default:
+            return 0;  // shouldn't happen, but avoids warnings.
+    }
+}
+
+
+Buffer *system_image_load(const char *path) {
+    SDL_Surface *tex_surf = IMG_Load(path);
+    check(tex_surf,
+            "Error loading texture. IMG_GetError(): %s\n", IMG_GetError());
+
+    Buffer *t = buffer_create(tex_surf->w, tex_surf->h);
+
+    for (int y = 0; y < tex_surf->h; y++) {
+        for (int x = 0; x < tex_surf->w; x++) {
+            uint32_t pixel = pixel_get(tex_surf, x, y);
+
+            uint8_t red, green, blue;
+            SDL_GetRGB(pixel, tex_surf->format, &red, &green, &blue);
+
+            t->pixels[y * t->width + x] = BUILDRGB(red, green, blue);
+        }
     }
 
-    fseek(f, 0, SEEK_END);
-    size_t fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);
+    SDL_FreeSurface(tex_surf);
 
-    char *buf = malloc(fsize + 1);
-    fread(buf, fsize, 1, f);
-    buf[fsize] = '\0';
-
-    fclose(f);
-
-    return buf;
-}
-
-
-static GLuint CompileShaderProgram() {
-    char *vertexSource = ReadFile("vertex.glsl");
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, (const GLchar **)&vertexSource, NULL);
-    glCompileShader(vertexShader);
-    free(vertexSource);
-
-    char *fragmentSource = ReadFile("frag.glsl");
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, (const GLchar **)&fragmentSource, NULL);
-    glCompileShader(fragmentShader);
-    free(fragmentSource);
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    return shaderProgram;
-}
-
-
-static GLuint BuildQuad(GLuint shaderProgram) {
-    GLfloat vertices[] = {
-        // Vertex coord  Texture coord
-        1,  1,           1, 0,
-        -1,  1,          0, 0,
-        1, -1,           1, 1,
-        -1, -1,          0, 1
-    };
-
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Vertex attributes
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
-            0);
-    glEnableVertexAttribArray(posAttrib);
-
-    GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
-    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
-            (void *)(2 * sizeof(float)));
-    glEnableVertexAttribArray(texAttrib);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    return vao;
-}
-
-
-static GLuint BuildTexture(int width, int height) {
-    GLuint tex;
-
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
-            GL_UNSIGNED_BYTE, NULL);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    return tex;
-}
-
-
-static void InitOpenGL(int width, int height) {
-    GLuint shaderProgram = CompileShaderProgram();
-    GLuint quad_vao = BuildQuad(shaderProgram);
-    GLuint tex = BuildTexture(width, height);
-
-    glUseProgram(shaderProgram);
-    glBindVertexArray(quad_vao);
-    glBindTexture(GL_TEXTURE_2D, tex);
-}
-
-
-GLFWwindow *system_window_init(int width, int height) {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-    // Create window and OpenGL context
-    GLFWwindow *window = glfwCreateWindow(width, height, "engine", NULL, NULL);
-    glfwMakeContextCurrent(window);
-
-    // Disable cursor
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-
-    // Init GLEW
-    glewExperimental = GL_TRUE;
-    glewInit();
-
-    InitOpenGL(width, height);
-
-    return window;
+    return t;
 }
 
 
 uint32_t system_time_get() {
-    return 1000 * glfwGetTime();
+    return SDL_GetTicks();
 }
 
 
-uint32_t system_blit(GLFWwindow *window, Buffer *buf) {
-    uint32_t start = system_time_get();
-
-    glTexSubImage2D(
-            GL_TEXTURE_2D,              // target
-            0,                          // level of detail
-            0, 0,                       // xoffset, yoffset
-            buf->width, buf->height,    // width, height
-            GL_RGBA, GL_UNSIGNED_BYTE,  // format, type
-            buf->pixels                 // pixels
-            );
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glfwSwapBuffers(window);
-
-    return system_time_get() - start;
+void system_sleep(uint32_t ms) {
+    SDL_Delay(ms);
 }
--- src/system.h
+++ src/system.h
@@ -7,12 +7,75 @@
 
 #include <stdint.h>
 
-#include <GLFW/glfw3.h>
-
 #include "buffer.h"
+#include "geometry.h"
 
-GLFWwindow *system_window_init(int width, int height);
+
+//------------------------------------------------------------------------------
+// Init / Shutdown
+//------------------------------------------------------------------------------
+
+// Initializes SDL. Creates a resizable window and handles resize events.
+void system_init(const char *title, int width, int height);
+
+// Just calls SDL_Quit.
+void system_quit();
+
+
+
+//------------------------------------------------------------------------------
+// Video
+//------------------------------------------------------------------------------
+
+// Set to 1 to set fullscreen.
+void system_fullscreen(int flag);
+
+// Update the screen with the contents of buf. Returns the time it took in ms.
+uint32_t system_blit(Buffer *buf);
+
+
+
+//------------------------------------------------------------------------------
+// Input
+//------------------------------------------------------------------------------
+
+// A Tick contains all the input info needed to process one gametick.
+typedef struct Tick {
+    int forward;            // 1 forward, -1 backwards
+    int strafe;             // 1 right, -1 left
+    int turn;               // 1 clockwise, -1 anticlockwise
+    int relative_mouse_x;   // > 0 clockwise, < 0 anticlockwise
+} Tick;
+
+
+// Returns a vector with the position of the mouse in buffer coordinates.
+Vector system_mousepos_get(Buffer *buf);
+
+// Set to 1 to keep the mouse inside the window.
+void system_mouse_grab(int flag);
+
+Tick system_tick_get();
+
+
+
+//------------------------------------------------------------------------------
+// Image loading
+//------------------------------------------------------------------------------
+
+// Load the image given by path into a Buffer.
+Buffer *system_image_load(const char *path);
+
+
+
+//------------------------------------------------------------------------------
+// Time
+//------------------------------------------------------------------------------
+
+// Returns the time since S_Init() in milliseconds.
 uint32_t system_time_get();
-uint32_t system_blit(GLFWwindow *window, Buffer *buf);
+
+// Sleep for at least ms milliseconds.
+// Count on a granularity of at least 10 millisecods.
+void system_sleep(uint32_t ms);
 
 #endif
