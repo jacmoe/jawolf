9be790ccdacc2ae4a5a93e31d9a2915dc60cdd3e
Jacob Moen <jacmoe.dk@gmail.com>
2016-12-03 13:17:40 +0100

    sdfsdf

--- src/gui.h
+++ src/gui.h
@@ -2,6 +2,13 @@
 #define GUI_H
 
 #include <GLFW/glfw3.h>
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
 
 void gui_init(GLFWwindow *window);
 void gui_fonts();
