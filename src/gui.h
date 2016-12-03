a7d0d547fddad1a0ab93ea1d775f32911127fe26
Jacob Moen <jacmoe.dk@gmail.com>
2016-12-03 12:31:49 +0100

    ssdfasf

--- src/gui.h
+++ src/gui.h
@@ -1,19 +1,7 @@
 #ifndef GUI_H
 #define GUI_H
 
-#include <stdint.h>
-
 #include <GLFW/glfw3.h>
-
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-
-#include "3rd_party/nuklear.h"
 
 void gui_init(GLFWwindow *window);
 void gui_fonts();
