d7fef8c2784a8c8404cfb331dc027018fde7a82e
Jacob Moen <jacmoe.dk@gmail.com>
2017-05-21 20:29:11 +0200

    Block and Block_map added

--- src/block.c
+++ src/block.c
@@ -1,4 +1,16 @@
-//
-// Created by jacmoe on 5/21/17.
-//
+/*
+*   This file is part of the
+*    _                     _  __
+*   (_) __ ___      _____ | |/ _|
+*   | |/ _` \ \ /\ / / _ \| | |_
+*   | | (_| |\ V  V / (_) | |  _|
+*  _/ |\__,_| \_/\_/ \___/|_|_|
+* |__/
+*
+*   project : https://github.com/jacmoe/jawolf
+*
+*   Copyright 2016 - 2017 Jacob Moen
+*
+*/
+#include "block.h"
 
--- src/block_map.h
+++ src/block_map.h
@@ -1,8 +1,19 @@
-//
-// Created by jacmoe on 5/21/17.
-//
-
+/*
+*   This file is part of the
+*    _                     _  __
+*   (_) __ ___      _____ | |/ _|
+*   | |/ _` \ \ /\ / / _ \| | |_
+*   | | (_| |\ V  V / (_) | |  _|
+*  _/ |\__,_| \_/\_/ \___/|_|_|
+* |__/
+*
+*   project : https://github.com/jacmoe/jawolf
+*
+*   Copyright 2016 - 2017 Jacob Moen
+*
+*/
 #ifndef JAWOLF_BLOCK_MAP_H
 #define JAWOLF_BLOCK_MAP_H
+
 
 #endif //JAWOLF_BLOCK_MAP_H
